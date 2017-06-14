57c586e5cdf4ea9d0d3e3a5f3985a056c05aa529
bmckalip <brmckalip@gmail.com>
2017-06-13 23:43:48 -0400

    finished rudimentary cycling of cards and progress bar animation

--- javascript/flashcards.js
+++ javascript/flashcards.js
@@ -1,19 +1,20 @@
 var sets = [];
 var currCardIndex;
+var currSet = {};
 var xhr = new XMLHttpRequest();
 window.onload = function(){
 	loadSets();
 }
 
 function loadSets(){
-	var urls = ["https://api.myjson.com/bins/gj9tn"];
+	var urls = ["https://api.myjson.com/bins/r8wjf"];
 	for (i in urls){
 		requestSet(urls[i]);
 	}
 	
 	//chevron event listners
-	document.getElementById("prevCard").addEventListener("click", nextCard);
-	document.getElementById("nextCard").addEventListener("click", prevCard);
+	document.getElementById("prevCard").addEventListener("click", prevCard);
+	document.getElementById("nextCard").addEventListener("click", nextCard);
 	
 	//flip button
 	document.getElementById("flipBtn").addEventListener("click", flipCard);
@@ -52,38 +53,86 @@ function addTab(cardSet){
 
 function addTabListeners(){
 	for(i in sets){
-		document.getElementById(sets[i].name).addEventListener("click", function(){currSet=set[i]; currCardIndex = 0; displaySet(sets[i].name, currCardIndex)});
+		document.getElementById(sets[i].name).addEventListener("click", function(){currSet=sets[i]; currCardIndex = -1; displaySet(sets[i], currCardIndex)});
 	}
 }
 
-function displaySet(setName, position){
-	var head = document.getElementById("cardHead").innerHTML;
-	var body = document.getElementById("cardBody").innerHTML;
-	loadCard(setName, position);
+function displaySet(set, position){
+	loadCard(set, position);
 }
 
 function nextCard(){
+	try{
+		if(currCardIndex < currSet.cards.length){
+			currCardIndex++;
+			loadCard(currSet, currCardIndex);
+		}
+		console.log("next: " + currCardIndex + " / " + currSet.cards.length);
+	}catch(TypeError){}
 	
 }
 
 function prevCard(){
-	
+	if(currCardIndex > -1){
+		currCardIndex--;
+		loadCard(currSet, currCardIndex);
+	}
+	console.log("prev: " + currCardIndex);
 }
 
-function loadCard(){
+function loadCard(set, position){
+	var head = document.getElementById("cardHead");
+	var body = document.getElementById("cardBody");
+	var card = set.cards[position];
 	
+	console.log(card);
+	//progress bar
+	setProgressBar((position + 1), set.cards.length);
+	if(position <= -1){
+		head.innerHTML = set.name;
+		body.innerHTML = "Press the next arrow to begin!"
+	}else if(position >= set.cards.length){
+		head.innerHTML = set.name;
+		body.innerHTML = "Press the Restart button to begin again!"
+	}else{
+		head.innerHTML = "Question";
+		try{
+			body.innerHTML = card.question;
+		}catch(TypeError){
+			
+		}		
+	}
 }
 
 function flipCard(){
-	console.log("flipping");
+	var head = document.getElementById("cardHead");
+	var body = document.getElementById("cardBody");
+	
+	if(currCardIndex >= 0 && currCardIndex <= currSet.cards.length){
+		var card = currSet.cards[currCardIndex];
+		console.log("flipping");
+	}
+	if(head.innerHTML.toLowerCase().includes("question")){
+		head.innerHTML = "Answer";
+		body.innerHTML = card.answer;
+	}else if(head.innerHTML.toLowerCase().includes("answer")){
+		head.innerHTML = "Question";
+		console.log(card);
+		body.innerHTML = card.question;
+	}
+
 }
 
 function setProgressBar(complete, total){
 	var bar = document.getElementById("progressBar");
-	var percentage = complete / total;
+	var percentage = complete / total * 100;
+	console.log(complete + " / " + total + " | " + percentage);
+	
 	//only add text value if it will "fit" (> 3% of the bar is filled)
 	if(percentage > 5){
 		bar.innerHTML = complete + "/" + total;
+	}else{
+		bar.innerHTML = "";
 	}
 	//set the width of bar
 	bar.setAttribute("style", "width: " + percentage + "%");
